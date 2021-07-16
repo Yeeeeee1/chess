ad590d196bb8ca7116aebdeff9c84209029a4b80
Konstantin <KostyaEK2006@gmail.com>
2021-07-16 22:22:15 +0300

    feat: add new server

--- src/js/server.js
+++ src/js/server.js
@@ -1,65 +1,18 @@
-/**
-Перед запуском:
-> npm install ws
-Далее:
-> node server.js
-> откройте http://localhost:8080 в вашем браузере
-*/
+const WebSocket = require("ws");
 
-const http = require("http");
-const fs = require("fs");
-const ws = new require("ws");
+const webSocketServer = new WebSocket.Server({ port: 8080 });
 
-const wss = new ws.Server({ noServer: true });
+webSocketServer.on("connection", (webSocket) => {
+  webSocket.on("message", (message) => {
+    console.log("Received:", message);
+    broadcast(message);
+  });
+});
 
-const clients = new Set();
-
-function accept(req, res) {
-  if (
-    req.url == "/ws" &&
-    req.headers.upgrade &&
-    req.headers.upgrade.toLowerCase() == "websocket" &&
-    // может быть подключён: keep-alive, Upgrade
-    req.headers.connection.match(/\bupgrade\b/i)
-  ) {
-    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
-  } else if (req.url == "/") {
-    // index.html
-    fs.createReadStream("./src/index.html").pipe(res);
-  } else {
-    // страница не найдена
-    res.writeHead(404);
-    res.end();
-  }
-}
-
-function onSocketConnect(ws) {
-  clients.add(ws);
-  log(`новое подключение`);
-
-  ws.on("message", function (message) {
-    log(`получено сообщение: ${message}`);
-
-    message = message.slice(0, 50); // максимальная длина сообщения 50
-
-    for (let client of clients) {
-      client.send(message);
+function broadcast(data) {
+  webSocketServer.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
     }
   });
-
-  ws.on("close", function () {
-    log(`подключение закрыто`);
-    clients.delete(ws);
-  });
-}
-
-let log;
-if (!module.parent) {
-  log = console.log;
-  http.createServer(accept).listen(8080);
-} else {
-  // для размещения на javascript.info
-  log = function () {};
-  // log = console.log;
-  exports.accept = accept;
 }
